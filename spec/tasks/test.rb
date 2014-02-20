3c90653a07baf02d910cdd02fa1866ce7d9d77cd
Brendan Caffrey <brendan@jcaffrey.com>
2014-02-20 15:27:43 -0600

    adding models and a test for the espn scraper

--- spec/tasks/test.rb
+++
@@ -1,9 +0,0 @@
-require 'spec/spec_helper'
-
-describe TestParse
-  it 'should not have an empty data' do
-    dataParser = Parser.new
-    data = dataParser.parse
-    expect(data).to.not eq('')
-  end
-end
