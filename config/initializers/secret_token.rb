68349608b27f0184c03015b1cf3955c1aac5ffc4
Brendan Caffrey <brendan@jcaffrey.com>
2014-02-19 20:21:31 -0600

    adding the secret token to the repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarchMadness::Application.config.secret_key_base = 'd7ddcbfabfdaa0b793b9c940a0409f3a9d7e06b1d1a07b1b9f861bb8d0feffeb40b339af1c6023a9949a10845e0b20810458e9bffeee70dc2c82a7347096c530'
