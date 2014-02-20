ec5626f3d6d2b378b9c3baf2bff537018b9a93fc
Brendan Caffrey <brendan@jcaffrey.com>
2014-02-20 15:39:35 -0600

    building database seeds so the UI can be tested without stressing the ESPN api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+conferences = {'Big 10' => ['Illinois', 'Indiana', 'Purdue', 'Ohio State', 'Michigan State', 'Iowa', 'Michigan',
+                            'Penn State', 'Wisconsin', 'Minnesota', 'Nebraska', 'Northwestern'],
+               'SEC' => ['Florida', 'Kentucky', 'Georgia', 'Missouri', 'LSU', 'Ole Miss', 'Tennessee', 'Arkansas',
+                         'Vanderbilt', 'Texas A&M', 'Alabama', 'Auburn', 'Mississippi State', 'South Carolina']}
+
+Conference.delete_all
+Team.delete_all
+
+conferences.each do |conf_name, teams|
+  conf = Conference.new(name: conf_name)
+  teams.each do |team_name|
+    Team.create(name: team_name, conference: conf)
+  end
+end
---
+++ lib/tasks/espn.rake
@@ -0,0 +1,22 @@
+require 'espn'
+
+namespace :espn do
+  desc 'Flush conference and team data from the database and pull in new teams'
+  task :scrape => :environment do
+    Conference.delete_all
+    Team.delete_all
+
+    client = ESPN::Client.new(api_key: '8eg4bt5zm92mvwjvne5agngr')
+    leagues = client.sports('basketball', 'mens-college-basketball')
+    confs = leagues[0].leagues[0][:groups][0][:groups]
+
+    confs.each { |c|
+      conf = Conference.create(name: c.shortName)
+
+      teams = client.teams('basketball', 'mens-college-basketball', group: c.groupId)	
+      teams.each { |t|
+        Team.create(name: t.nickname, conference: conf)
+      }
+    }
+  end
+end
