9429e088d155115fc2d3e7f1fc4867d001f289c7
Dan Schweikert <schweikert.dan@gmail.com>
2014-03-17 20:31:02 -0500

    Removing the schedules table

--- app/helpers/espn_scraper_helper.rb
+++ app/helpers/espn_scraper_helper.rb
@@ -268,68 +268,6 @@ module EspnScraperHelper
     end 
   end
 
-=begin
-  # Loops through all teams and calls get_team_schedule
-  def get_all_schedules
-    Schedule.delete_all
-
-    Team.find_each do |team|
-      get_team_schedule(team)
-      sleep 15.0
-    end
-  end
-
-  # Gets a team's schedule and adds to database
-  # 	name - team name
-  #	id - ESPN id for the team
-  def get_team_schedule (team)
-    #Set Up
-    url = "http://espn.go.com/mens-college-basketball/team/schedule/_/id/#{team.webExt}"
-    agent = Mechanize.new
-    html = agent.get(url).body
-    doc = Nokogiri::HTML(html)
-    table = doc.xpath('//table[@class = "tablehead"]')
-    rows = table.css('tr')
-
-    #Loop through each row
-    rows.each  do |row|
-      if(row.css('li').count > 3)
-        #get Date
-        date = row.css("td")[0].text
-
-        #get HOME/away (vs/@)
-        loc = row.css('li[@class ="game-status"]').text
-
-        #get opp
-        opp = row.css('li[@class ="team-name"]').text
-
-        #get win / loss
-        result = false
-        win = row.css('li[@class ="game-status win"]').text 
-        if(!win.empty?)
-          result = true
-        end
-        #maybe check for false???
-        #loss = row.css('li[@class ="game-status loss"]').text 
-
-        #get score
-        scores = row.css('li[@class ="score"]').text.scan((/(\d+)-(\d+)/))
-        tScore = 0
-        oScore = 0
-        if(result)
-          tScore = scores[0][0].to_i
-          oScore = scores[0][1].to_i
-        else
-          tScore = scores[0][1].to_i
-          oScore = scores[0][0].to_i
-        end
-
-        Schedule.create(date: date, location: loc, opponent: opp, isWinner: result, teamScore: tScore, oppScore: oScore, team: team)
-      end
-    end
-  end
-=end
-
   def get_games
     Game.delete_all
     Team.find_each do |team|
@@ -372,7 +310,7 @@ module EspnScraperHelper
             game = Game.create(gameID: gameID, date: date, homeTeam: homeTeam, awayTeam: awayTeam)
             get_game_stats(game)
 
-            sleep 2.0
+            sleep 5.0
           end
         end
       end
--- spec/tasks/espnScraper_spec.rb
+++ spec/tasks/espnScraper_spec.rb
@@ -4,11 +4,13 @@ require "#{Rails.root}/app/helpers/espn_scraper_helper"
 include EspnScraperHelper
 
 describe EspnScraperHelper do
+
   before(:all) do
-    Conference.delete_all
-    Team.delete_all
-    Game.delete_all
-    EspnScraperHelper.get_confs()
+      Conference.delete_all
+      Team.delete_all
+      Game.delete_all
+
+      EspnScraperHelper.get_confs()
   end
 
   describe '.get_confs()' do
@@ -16,10 +18,84 @@ describe EspnScraperHelper do
 
       it 'Conference should contain 33 entries' do
         assert(Conference.all.count == 33)
+      end    
+      it 'all conferences should have a name' do
+        assert(Conference.where(name: nil).count == 0)
+      end
+      it 'all conferences should have a webExt' do
+        assert(Conference.where(webExt: nil).count == 0)
+      end
+      it 'all conferences should have a logo' do
+        assert(Conference.where(logo: nil).count == 0)
+      end
+
+      bigTen = Conference.find_by! name: 'Big Ten'
+      it 'conference should contain "Big Ten"' do
+        assert(bigTen != nil)
+      end 
+      it '"Big Ten" should have the correct web extension' do
+        assert(bigTen.webExt = 'http://espn.go.com/ncb/conference?confId=7')
+      end 
+      it '"Big Ten" should have the correct logo' do
+        assert(bigTen.logo = 'http://a.espncdn.com/i/teamlogos/ncaa_conf/sml/trans/big_ten.gif')
       end 
 
-    
-   
+      acc = Conference.find_by! name: 'ACC'
+      it 'conference should contain "ACC"' do
+        assert(acc != nil)
+      end 
+      it '"ACC" should have the correct web extension' do
+        assert(acc.webExt = '/ncb/conference?confId=2')
+      end 
+      it '"ACC" should have the correct logo' do
+        assert(acc.logo = 'http://a.espncdn.com/i/teamlogos/ncaa_conf/sml/trans/acc.gif')
+      end 
+    end
+  end
+
+  describe '.get_teams_from_conf(conf)' do
+    describe 'scrape ESPN and populate Teams from the given conference' do
+      acc = Conference.find_by! name: 'ACC'
+      get_teams_from_conf(acc)
+      bigTen = Conference.find_by! name: 'Big Ten'
+      get_teams_from_conf(bigTen)
+  
+      it 'all teams should have a name' do
+        assert(Team.where(name: nil).count == 0)
+      end
+      it 'all teams should have a rank' do
+        assert(Team.where(rank: nil).count == 0)
+      end
+      it 'all teams should have a webExt' do
+        assert(Team.where(webExt: nil).count == 0)
+      end
+      it 'all teams should have wins' do
+        assert(Team.where(wins: nil).count == 0)
+      end
+      it 'all teams should have losses' do
+        assert(Team.where(losses: nil).count == 0)
+      end
+      it 'all teams should have a conference' do
+        assert(Team.where(conference: nil).count == 0)
+      end
+
+      it 'Big 10 should contain 12 entries and ACC should contain 15 entries' do
+        assert(Team.all.count == 27)
+      end  
+      illinois = Team.find_by! name: 'Illinois'
+      it 'Teams should contain "Illinois"' do
+        assert(illinois != nil)
+      end 
+      it '"Illinois" should have the correct web extension' do
+        assert(illinois.webExt = '356/illinois-fighting-illini')
+      end 
+      duke = Team.find_by! name: 'Duke'
+      it 'temp_teams should contain "Duke"' do
+        assert(duke != nil)
+      end 
+      it '"Duke" should have the correct web extension' do
+        assert(duke.webExt = '150/duke-blue-devils')
+      end
     end
   end
 
