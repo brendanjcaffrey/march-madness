b87420ee6e6fdf08cca3d7244dd62f8978488d89
Dan Schweikert <schweikert.dan@gmail.com>
2014-03-18 13:20:35 -0500

    Made more tests

--- spec/tasks/espnScraper_spec.rb
+++ spec/tasks/espnScraper_spec.rb
@@ -4,7 +4,9 @@ require "#{Rails.root}/app/helpers/espn_scraper_helper"
 include EspnScraperHelper
 
 describe EspnScraperHelper do
-=begin
+
+  sleepTime = 4.0
+
   describe '.get_confs()' do
     describe 'scrape ESPN and populate Conferences' do
 
@@ -16,7 +18,8 @@ describe EspnScraperHelper do
         Team.delete_all
         Game.delete_all
 
-        EspnScraperHelper.get_confs()
+        get_confs()
+        sleep sleepTime
         bigTen = Conference.find_by! name: 'Big Ten'
         acc = Conference.find_by! name: 'ACC'
       end
@@ -67,11 +70,14 @@ describe EspnScraperHelper do
         Team.delete_all
         Game.delete_all
 
-        EspnScraperHelper.get_confs()
+        get_confs()
+        sleep sleepTime
         bigTen = Conference.find_by! name: 'Big Ten'
         acc = Conference.find_by! name: 'ACC'
         get_teams_from_conf(bigTen)
+        sleep sleepTime
         get_teams_from_conf(acc)
+        sleep sleepTime
         illinois = Team.find_by! name: 'Illinois'
         duke = Team.find_by! name: 'Duke'
       end
@@ -108,19 +114,22 @@ describe EspnScraperHelper do
       end
     end
   end
-=end
+
   describe '.get_team_scoring_stats(conf)' do
-    describe 'scrape ESPN and populate Teams from the given conference' do
+    describe 'scrape ESPN and update Teams with scoring stats' do
 
       before(:all) do
         Conference.delete_all
         Team.delete_all
         Game.delete_all
 
-        EspnScraperHelper.get_confs()
+        get_confs()
+        sleep sleepTime
         bigTen = Conference.find_by! name: 'Big Ten'
         get_teams_from_conf(bigTen)
+        sleep sleepTime
         get_team_scoring_stats(bigTen)
+        sleep sleepTime
       end
 
       it 'all teams should have points' do
@@ -152,58 +161,253 @@ describe EspnScraperHelper do
   end
 
   describe 'get_team_adv_scoring_stats(conf)' do
-    describe 'scrape ESPN and populate Teams from the given conference' do
+    describe 'scrape ESPN and update Teams with advanced scoring stats' do
 
       before(:all) do
         Conference.delete_all
         Team.delete_all
         Game.delete_all
 
-        EspnScraperHelper.get_confs()
+        get_confs()
+        sleep sleepTime
         bigTen = Conference.find_by! name: 'Big Ten'
         get_teams_from_conf(bigTen)
+        sleep sleepTime
         get_team_adv_scoring_stats(bigTen)
+        sleep sleepTime
       end
 
+      it 'all teams should have a two point percentage' do
+        assert(Team.where(twoPer: nil).count == 0)
+      end
+      it 'all teams two point percentage is between 0 and 1' do
+        assert(Team.where('twoPer < 0').count == 0 && Team.where('fgPer > 1').count == 0)
+      end
+      it 'all teams should have an adjusted field goal percentage' do
+        assert(Team.where(adjFG: nil).count == 0)
+      end
+      it 'all teams adjusted field goal percentage is between 0 and 1' do
+        assert(Team.where('adjFG < 0').count == 0 && Team.where('adjFG > 1').count == 0)
+      end
+      it 'all teams should have points per shot' do
+        assert(Team.where(pps: nil).count == 0)
+      end
+      it 'all teams points per shot is greater than 0' do
+        assert(Team.where('pps < 0').count)
+      end
     end
   end
-=begin
- 
-#number of games for illinois 29
-#one game for illinois (all stats)
-  describe '.get_teams' do
-    describe 'scrape ESPN and populate temp_teams with all teams and web extensions' do
-  
-  EspnScraperHelper.get_team_schedule(TempTeam.find_by! name: 'Illinois')
-      it 'schedules is not empty' do
-        assert(Schedule.all != nil)
+
+  describe 'get_team_assists_stats(conf)' do
+    describe 'scrape ESPN and update Teams with assist stats' do
+
+      before(:all) do
+        Conference.delete_all
+        Team.delete_all
+        Game.delete_all
+
+        get_confs()
+        sleep sleepTime
+        bigTen = Conference.find_by! name: 'Big Ten'
+        get_teams_from_conf(bigTen)
+        sleep sleepTime
+        get_team_assists_stats(bigTen)
+        sleep sleepTime
       end
 
-      it 'testing Illinois played the correct number of games (less than 35 due to games being played)' do
-        assert(Schedule.all.count > 35)
+      it 'all teams should have a assists per game' do
+        assert(Team.where(assist: nil).count == 0)
       end
-
-      it 'ensure that games contain correct details' do
-        assert(Schedule.where(date: nil).count == 0)
-        assert(Schedule.where(location: nil).count == 0)
-        assert(Schedule.where(opponent: nil).count == 0)
-        assert(Schedule.where(isWinner: nil).count == 0)
-        assert(Schedule.where(teamScore: nil).count == 0)
-        assert(Schedule.where(oppScore: nil).count == 0)
-        assert(Schedule.where(temp_team_id: nil).count == 0)
+      it 'all teams should have turnover per game' do
+        assert(Team.where(to: nil).count == 0)
       end
-
-      it 'stats are correct for Illinois first game' do
-        game = Schedule.first
-        assert(game.date = "Fri, Nov 8")
-        assert(game.location == "vs")
-        assert(game.opponent == "Alabama State")
-        assert(game.isWinner)
-        assert(game.teamScore == 80)
-        assert(game.oppScore == 63)
+      it 'all teams should have assists per turnover' do
+        assert(Team.where(apto: nil).count == 0)
       end
-
     end
   end
-=end
+
+  describe 'get_team_rebounds_stats(conf)' do
+    describe 'scrape ESPN and update Teams with rebounding stats' do
+
+      before(:all) do
+        Conference.delete_all
+        Team.delete_all
+        Game.delete_all
+
+        get_confs()
+        sleep sleepTime
+        bigTen = Conference.find_by! name: 'Big Ten'
+        get_teams_from_conf(bigTen)
+        sleep sleepTime
+        get_team_rebounds_stats(bigTen)
+        sleep sleepTime
+      end
+
+      it 'all teams should have a offensive rebounds per game' do
+        assert(Team.where(offReb: nil).count == 0)
+      end
+      it 'all teams should have defensive rebounds per game' do
+        assert(Team.where(defReb: nil).count == 0)
+      end
+      it 'all teams should have rebounds per game' do
+        assert(Team.where(totalReb: nil).count == 0)
+      end
+    end
+  end
+
+  describe 'get_team_steals_stats(conf)' do
+    describe 'scrape ESPN and update Teams with steal stats' do
+
+      before(:all) do
+        Conference.delete_all
+        Team.delete_all
+        Game.delete_all
+
+        get_confs()
+        sleep sleepTime
+        bigTen = Conference.find_by! name: 'Big Ten'
+        get_teams_from_conf(bigTen)
+        sleep sleepTime
+        get_team_steals_stats(bigTen)
+        sleep sleepTime
+      end
+
+      it 'all teams should have a steals per game' do
+        assert(Team.where(steals: nil).count == 0)
+      end
+      it 'all teams should have fouls per game' do
+        assert(Team.where(fouls: nil).count == 0)
+      end
+      it 'all teams should have steals per turnover per game' do
+        assert(Team.where(stealPerTO: nil).count == 0)
+      end
+      it 'all teams should have steals per foul per game' do
+        assert(Team.where(stealPerFoul: nil).count == 0)
+      end
+    end
+  end
+
+  describe 'get_team_blocks_stats(conf)' do
+    describe 'scrape ESPN and update Teams with block stats' do
+
+      before(:all) do
+        Conference.delete_all
+        Team.delete_all
+        Game.delete_all
+
+        get_confs()
+        sleep sleepTime
+        bigTen = Conference.find_by! name: 'Big Ten'
+        get_teams_from_conf(bigTen)
+        sleep sleepTime
+        get_team_blocks_stats(bigTen)
+        sleep sleepTime
+      end
+
+      it 'all teams should have a blocks per game' do
+        assert(Team.where(blocks: nil).count == 0)
+      end
+      it 'all teams should have block per foul per game' do
+        assert(Team.where(blocksPerFoul: nil).count == 0)
+      end
+    end
+  end
+
+  describe 'get_game_stats(game)' do
+    describe 'scrape ESPN and update a game with given stats for that game' do
+
+      game = nil
+
+      before(:all) do
+        Conference.delete_all
+        Team.delete_all
+        Game.delete_all
+
+        game = Game.create(gameID: 400510014)
+        get_game_stats(game)
+        sleep sleepTime
+      end
+
+      it 'games should contain home team points' do
+        assert(Game.where(homePoints: nil).count == 0)
+      end
+      it 'games should contain home team field goal percentage' do
+        assert(Game.where(homefgPer: nil).count == 0)
+      end
+      it 'home team field goal percentage is between 0 and 1' do
+        assert(Game.where('homefgPer < 0').count == 0 && Game.where('homefgPer > 1').count == 0)
+      end
+      it 'games should contain home team three point percentage' do
+        assert(Game.where(homethreePer: nil).count == 0)
+      end
+      it 'home team three point percentage is between 0 and 1' do
+        assert(Game.where('homethreePer < 0').count == 0 && Game.where('homethreePer > 1').count == 0)
+      end
+      it 'games should contain home team free throw percentage' do
+        assert(Game.where(homeftPer: nil).count == 0)
+      end
+      it 'home team free throw percentage is between 0 and 1' do
+        assert(Game.where('homeftPer < 0').count == 0 && Game.where('homeftPer > 1').count == 0)
+      end
+      it 'games should contain home team rebounds' do
+        assert(Game.where(hometotalReb: nil).count == 0)
+      end
+      it 'games should contain home team assists' do
+        assert(Game.where(homeassist: nil).count == 0)
+      end
+      it 'games should contain home team turnovers' do
+        assert(Game.where(hometo: nil).count == 0)
+      end
+      it 'games should contain home team steals' do
+        assert(Game.where(homesteals: nil).count == 0)
+      end
+      it 'games should contain home team blocks' do
+        assert(Game.where(homeblocks: nil).count == 0)
+      end
+      it 'games should contain home team fouls' do
+        assert(Game.where(homefouls: nil).count == 0)
+      end
+
+      it 'games should contain away team points' do
+        assert(Game.where(awaypoints: nil).count == 0)
+      end
+      it 'games should contain away team field goal percentage' do
+        assert(Game.where(awayfgPer: nil).count == 0)
+      end
+      it 'away team field goal percentage is between 0 and 1' do
+        assert(Game.where('awayfgPer < 0').count == 0 && Game.where('awayfgPer > 1').count == 0)
+      end
+      it 'games should contain away team three point percentage' do
+        assert(Game.where(awaythreePer: nil).count == 0)
+      end
+      it 'away team three point percentage is between 0 and 1' do
+        assert(Game.where('awaythreePer < 0').count == 0 && Game.where('awaythreePer > 1').count == 0)
+      end
+      it 'games should contain away team free throw percentage' do
+        assert(Game.where(awayftPer: nil).count == 0)
+      end
+      it 'away team free throw percentage is between 0 and 1' do
+        assert(Game.where('awayftPer < 0').count == 0 && Game.where('awayftPer > 1').count == 0)
+      end
+      it 'games should contain away team rebounds' do
+        assert(Game.where(awaytotalReb: nil).count == 0)
+      end
+      it 'games should contain away team assists' do
+        assert(Game.where(awayassist: nil).count == 0)
+      end
+      it 'games should contain away team turnovers' do
+        assert(Game.where(awayto: nil).count == 0)
+      end
+      it 'games should contain away team steals' do
+        assert(Game.where(awaysteals: nil).count == 0)
+      end
+      it 'games should contain away team blocks' do
+        assert(Game.where(awayblocks: nil).count == 0)
+      end
+      it 'games should contain away team fouls' do
+        assert(Game.where(awayfouls: nil).count == 0)
+      end
+    end
+  end
 end
