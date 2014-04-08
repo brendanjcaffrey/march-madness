aed41a2efa245b858de0a1442260e7c1894b03e6
Brendan Caffrey <brendan@jcaffrey.com>
2014-04-08 02:26:36 -0500

    removing debugging code

--- features/step_definitions/backend.rb
+++ features/step_definitions/backend.rb
@@ -2,7 +2,6 @@ Given(/^the backend is connected$/) do
   p 'Start the backend tests now, waiting...'
   @queue = ServerConnection.new
   @queue.connect
-  #@queue = nil
 end
 
 When(/^"(.+)" and "(.+)" are sent to the backend$/) do |team1, team2|
