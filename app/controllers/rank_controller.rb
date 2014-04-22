d4aea6088520eb9555720db9b6dbe10156689a30
Brendan Caffrey <brendan@jcaffrey.com>
2014-04-21 21:52:19 -0500

    the rank can now be pulled from the backend

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -1,5 +1,11 @@
 class RankController < ApplicationController
   def root
-    @teams = Team.all
+    if Rails.env.production?
+      $backend.sendTeams(['rank'])
+      ranks = $backend.getRanking
+      @teams = ranks.map { |team| Team.where(name: team).first }
+    else
+      @teams = Team.all
+    end
   end
 end
