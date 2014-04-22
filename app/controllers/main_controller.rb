147c948e6bc15a11dad4daa7b59b5a8deb1de39d
Brendan Caffrey <brendan@jcaffrey.com>
2014-04-21 21:32:11 -0500

    fix two small bugs

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,9 +15,10 @@ class MainController < ApplicationController
       if team1 == nil or team2 == nil
         render :nothing
       else
-        $backend.sendTeams(team1.name, team2.name)
-        winner = $backend.getWinner
-        render json: {team1: params['team1'], team2: params['team2'], winner: winner == team1.name ? '1' : '2'}
+        $backend.sendTeams([team1.name, team2.name])
+        ranking = $backend.getRanking
+        winner = ranking.first
+        render json: {team1: params['team1'], team2: params['team2'], winner: winner == team1.name ? '1' : '2', debug: ranking}
       end
     else
       render json: {team1: params['team1'], team2: params['team2'], winner: '1'}
